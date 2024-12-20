a76e1ec6cf7d245750d919e564bf6aee0194939e
Luceta <57941164+Luceta@users.noreply.github.com>
2024-12-20 15:30:02 +0900

    chore: 로딩시 텍스트 추가

--- src/router/home.tsx
+++ src/router/home.tsx
@@ -27,42 +27,42 @@ declare global {
 export default function EmotionMain() {
   const { dateStr, handlers, activeDates, loading } = useHome();
 
-  console.log(loading);
-
-  if (!loading) {
-    return (
-      <Box p={5} bgColor="mainBg" h="100vh">
-        <Flex direction="column" gap="10px">
-          <Container p={0}>
-            <VStack spacing="9px" align="stretch">
-              {/*<EmotionBox score={89} change={-5.2} />*/}
-              <MainCalendar
-                onDateSelect={handlers.handleDateSelect}
-                onViewChange={handlers.setMonthStr}
-                activeDates={activeDates}
-              />
-            </VStack>
-          </Container>
-          <HStack spacing="9px">
-            <DiaryButton onClick={handlers.handleDiary} icon={<IconPen />}>
-              <Flex>
-                <Text lineHeight={"25px"}>일기 쓰기</Text>
-                <IconCalChevronRight />
-              </Flex>
-            </DiaryButton>
-            <DiaryButton
-              onClick={handlers.handleTrash}
-              color="white"
-              icon={<IconTrashCan />}
-            >
-              <Flex>
-                <Text lineHeight={"25px"}>감정 쓰레기통</Text>
-                <IconCalChevronRight />
-              </Flex>
-            </DiaryButton>
-          </HStack>
-        </Flex>
-      </Box>
-    );
+  if (loading) {
+    <Text>Loading..</Text>;
   }
+
+  return (
+    <Box p={5} bgColor="mainBg" h="100vh">
+      <Flex direction="column" gap="10px">
+        <Container p={0}>
+          <VStack spacing="9px" align="stretch">
+            {/*<EmotionBox score={89} change={-5.2} />*/}
+            <MainCalendar
+              onDateSelect={handlers.handleDateSelect}
+              onViewChange={handlers.setMonthStr}
+              activeDates={activeDates}
+            />
+          </VStack>
+        </Container>
+        <HStack spacing="9px">
+          <DiaryButton onClick={handlers.handleDiary} icon={<IconPen />}>
+            <Flex>
+              <Text lineHeight={"25px"}>일기 쓰기</Text>
+              <IconCalChevronRight />
+            </Flex>
+          </DiaryButton>
+          <DiaryButton
+            onClick={handlers.handleTrash}
+            color="white"
+            icon={<IconTrashCan />}
+          >
+            <Flex>
+              <Text lineHeight={"25px"}>감정 쓰레기통</Text>
+              <IconCalChevronRight />
+            </Flex>
+          </DiaryButton>
+        </HStack>
+      </Flex>
+    </Box>
+  );
 }
